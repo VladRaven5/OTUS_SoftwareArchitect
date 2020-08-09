cc3d8364de9feb42b6f84afb86de17ac2ad2223a
VladRaven5 <vladiksat@list.ru>
2020-08-09 14:24:56 +0700

    Added shared bg service, outbox sender base

---
+++ Graduation_project/src/Shared/Communications/OutboxMessagesSenderBase.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public abstract class OutboxMessagesSenderBase : BackgroundService
+    {
+        protected readonly long _defaulPeriodMsec = 1000;
+
+        public OutboxMessagesSenderBase()
+        {
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            Stopwatch sw = new Stopwatch();
+
+            while(true)
+            {
+                try
+                {                   
+                    if(stoppingToken.IsCancellationRequested)
+                        break;
+                    
+                    sw.Restart();
+
+                    bool isSomeMessagesHandled = await HandleOneMessageAsync();
+                    if(isSomeMessagesHandled)
+                    {
+                        Console.WriteLine($"Message sent");
+                    }
+                    
+                    sw.Stop();
+                    long msElapsed = sw.ElapsedMilliseconds;
+                    long msToAwait = _defaulPeriodMsec - msElapsed;
+                    Console.WriteLine($"Await new message {msToAwait} ms");
+                    if(!isSomeMessagesHandled && msToAwait > 0)
+                    {
+                        await Task.Delay((int)msToAwait);
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Exception during sending outbox\n{e.Message}\n{e.StackTrace}");
+                }                
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>true if some data handled</returns>
+        protected abstract Task<bool> HandleOneMessageAsync();
+    }
+}
