9fad5ff02aa0619ba7acf655b265e864cbeaf713
VladRaven5 <vladiksat@list.ru>
2020-08-10 02:03:32 +0700

    Added massive build-push scripts

---
+++ Graduation_project/src/run_push_all.sh
@@ -0,0 +1,7 @@
+docker-compose -f api-docker-compose.yml push
+docker-compose -f auth-docker-compose.yml push
+docker-compose -f labels-docker-compose.yml push
+docker-compose -f project-members-docker-compose.yml push
+docker-compose -f projects-docker-compose.yml push
+docker-compose -f users-docker-compose.yml push
+docker-compose -f working-hours-docker-compose.yml push
